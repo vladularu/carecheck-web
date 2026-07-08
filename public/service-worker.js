20942303caba840534fbdba062fed1dc587a7d49
vladularu <vladularu@live.com>
2026-07-09 01:38:05 +0200

    v1.1.0 CareCheck als PWA installierbar machen

---
+++ public/service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "carecheck-tvoed-v1-1-0";
+
+const CORE_ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/carecheck-icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then((cachedResponse) => {
+          return cachedResponse || caches.match("/");
+        })
+      )
+  );
+});
